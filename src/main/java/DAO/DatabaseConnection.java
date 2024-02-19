e234b676362bbd18bd9115ecccb5d0d33fbdaf93
Trí Minh <vominhtri13@gmail.com>
2024-02-19 13:39:17 +0700

    Make connection a singleton to improve performant

--- src/main/java/DAO/DatabaseConnection.java
+++ src/main/java/DAO/DatabaseConnection.java
@@ -4,6 +4,7 @@
  */
 package DAO;
 
+import javax.xml.crypto.Data;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,14 +13,26 @@ import java.sql.SQLException;
  * @author M S I
  */
 public class DatabaseConnection {
+    protected static Connection _conn;
     protected Connection conn;
-    
-    public void open() {
+
+    public static Connection getInstance() {
+        if (_conn == null) {
+            createConnection();
+        }
+
+        return _conn;
+    }
+
+
+
+
+    private static void createConnection() {
         try {
             // Đăng ký driver
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            this.conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=SMS;user=sa;password=ktpm@123;serverTimezone=UTC;encrypt=true;trustServerCertificate=true");
-            System.out.println("Kết nối thành công" + this.conn.getCatalog());
+            _conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=SMS;user=sa;password=ktpm@123;serverTimezone=UTC;encrypt=true;trustServerCertificate=true");
+            System.out.println("Kết nối thành công" + _conn.getCatalog());
 
         } catch (ClassNotFoundException ex) {
             System.out.println("Lỗi : " + ex.toString());
@@ -28,11 +41,16 @@ public class DatabaseConnection {
         }
     }
 
+
+    public void open() {
+        conn = DatabaseConnection.getInstance();
+    }
+
     public void close() {
-        try {
-            this.conn.close();
-        } catch (SQLException ex) {
-            System.out.println("Lỗi : " + ex.toString());
-        }
+//        try {
+//            this.conn.close();
+//        } catch (SQLException ex) {
+//            System.out.println("Lỗi : " + ex.toString());
+//        }
     }
 }
